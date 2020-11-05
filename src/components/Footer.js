4dd87d103a4cced4ae65362597ee582874ec8f28
Beasley <beasley.com@gmail.com>
2020-11-04 23:32:13 -0800

    added basic components

---
+++ src/components/Footer.js
@@ -0,0 +1,6 @@
+'use strict';
+export default function Footer(){
+    return (
+        <p className="footer">&copy; @beasleydotcom 2020</p>
+    )
+}
---
+++ src/components/TimeSlots.js
@@ -0,0 +1,21 @@
+'use strict';
+import logo from "../logo.svg";
+
+export default function (){
+    return (
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            This here is the content of TimeSlot.js
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn ReactFun!
+        </a>
+      </header>
+    )
+}
